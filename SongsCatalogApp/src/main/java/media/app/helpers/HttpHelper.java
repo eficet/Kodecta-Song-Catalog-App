909395187c2dcb2b498557907eb47398be08f43e
Fayiz Hamad <e.f.i_cet@hotmail.com>
2019-01-05 18:42:03 +0100

    Feat: Added HttpHelper and StringHelper

--- SongsCatalogApp/src/main/java/media/app/helpers/HttpHelper.java
+++ SongsCatalogApp/src/main/java/media/app/helpers/HttpHelper.java
@@ -1,11 +1,21 @@
 package media.app.helpers;
 
+import media.app.model.Song;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public class HttpHelper {
 
     public static ResponseEntity<?> getHttpResponseEntity (String message, HttpStatus status) {
         return new ResponseEntity<>(message, status);
+    }
+
+    public static ResponseEntity<?> getHttpResponseEntity (Song song, HttpStatus status) {
+        return new ResponseEntity<>(song, status);
+    }
+    public static ResponseEntity<?> getHttpResponseEntity (List<Song> songList, HttpStatus status) {
+        return new ResponseEntity<>(songList, status);
     }
 }
